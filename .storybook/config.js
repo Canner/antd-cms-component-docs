ade5cc2134e99c66cade0575ba56eeb9de3aebb2
chilijung <chilijung@gmail.com>
2018-05-11 17:43:05 +0800

    config canner url

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,18 @@
 // @flow
 import { addDecorator, configure } from '@storybook/react';
 import { setConsoleOptions } from '@storybook/addon-console';
+import {setOptions} from '@storybook/addon-options';
 
 setConsoleOptions({
   panelExclude: [/[HMR]/, /Warning/],
   log: 'onChange log'
+});
+
+setOptions({
+  showDownPanel: false,
+  name: 'canner-cms',
+  url: 'https://github.com/Canner/react-cms-components',
+  sidebarAnimations: true
 });
 
 function loadStories() {
