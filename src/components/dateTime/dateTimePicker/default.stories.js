4d4c196b6be3da7d79415a6026ff0de5c300b66e
Siou <abz53378@gmail.com>
2018-10-24 13:01:56 +0800

    datetim and image

--- src/components/dateTime/dateTimePicker/default.stories.js
+++ src/components/dateTime/dateTimePicker/default.stories.js
@@ -4,17 +4,28 @@ import RefId from 'canner-ref-id';
 import customPreview from 'stories/customPreview';
 import README from './README.md';
 import IntlComponent from 'components/IntlComponent';
+import {withKnobs, text, select} from '@storybook/addon-knobs';
 
 import Date from '@canner/antd-string-date_time_picker';
 import { storiesOf } from '@storybook/react';
 import ExamplePrimitiveValueWrapper from '../../ExamplePrimitiveValueHoc';
 
-
 const DatePicker = ExamplePrimitiveValueWrapper("2018-03-24T16:00:00.201Z")(IntlComponent(Date))
 
 storiesOf('DateTime', module)
-  .add('DateTime', customPreview(README, () => (
+  .addDecorator(withKnobs)
+  .addDecorator(customPreview(README))
+  .add('DateTime', () => (
     <DatePicker
       refId={new RefId("date")}
-      />
-  )));
+      uiParams={{
+        format: text('format', 'YYYY/MM/DD'),
+        output: select('output', [
+          'ISO_8601',
+          'timestamp.millseconds',
+          'timestamp.seconds',
+          'epoch'
+        ])
+      }}
+    />
+  ));
--- src/components/image/image/default.stories.js
+++ src/components/image/image/default.stories.js
@@ -4,23 +4,37 @@ import RefId from 'canner-ref-id';
 import customPreview from 'stories/customPreview';
 import README from './README.md';
 import IntlComponent from 'components/IntlComponent';
+import { withKnobs, number, text } from '@storybook/addon-knobs/react';
+import {ImgurStorage} from '@canner/storage';
 
 import Image from '@canner/antd-object-image';
 import { storiesOf } from '@storybook/react';
 import ExamplePrimitiveValueWrapper from '../../ExamplePrimitiveValueHoc';
 
-const value = {
-  url: "https://cdn.canner.io/images/logo/logo-word.png",
-  size: 0,
-  name: "",
-  contentType: ""
-};
-
-const ImageWrapper = ExamplePrimitiveValueWrapper(value)(IntlComponent(Image))
-
 storiesOf('Image', module)
-  .add('Image', customPreview(README, () => (
-    <ImageWrapper
-      refId={new RefId("image")}
+  .addDecorator(withKnobs)
+  .addDecorator(customPreview(README))
+  .add('Image', () => {
+    const value = {
+      url: "https://cdn.canner.io/images/logo/logo-word.png",
+      size: 0,
+      name: "",
+      contentType: ""
+    };
+    
+    const ImageWrapper = ExamplePrimitiveValueWrapper(value)(IntlComponent(Image))
+    
+    return (
+      <ImageWrapper
+        refId={new RefId("image")}
+        imageStorage={new ImgurStorage({
+          clientId: '<clientId>'
+        })}
+        uiParams={{
+          limitSize: number('limitSize', 6000),
+          dirname: text('dirname', ''),
+          filename: text('filename', '')
+        }}
       />
-  )));
+    )
+  });
